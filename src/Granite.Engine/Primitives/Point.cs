32632d6b9494e657ceacfaff92af2e7b2aa77603
andreijeman <andrei.jeman3@gmail.com>
2026-07-08 23:13:39 +0300

    feat: add primitives math operations helpers

--- src/Granite.Engine/Primitives/Point.cs
+++ src/Granite.Engine/Primitives/Point.cs
@@ -1,10 +1,23 @@
 namespace Granite.Engine.Primitives;
 
-public readonly record struct Point(int X, int Y)
+public readonly record struct Point
 {
-    public static Point operator +(Point p1, Point p2) => new(p1.X + p2.X, p1.Y + p2.Y);
-    public static Point operator -(Point p1, Point p2) => new(p1.X - p2.X, p1.Y - p2.Y);
+    public int Y { get; }
+    public int X { get; }
 
     public static Point Zero { get; } = new(0, 0);
     public static Point One { get; } = new(1, 1);
+
+    public static Point operator +(Point p1, Point p2) => new(p1.X + p2.X, p1.Y + p2.Y);
+    public static Point operator -(Point p1, Point p2) => new(p1.X - p2.X, p1.Y - p2.Y);
+    
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Rect ToRect(Size size) => new(this, size);
+    public Point Translate(Point offset) => this + offset;
+    public Point Rebase(Point oldOrigin, Point newOrigin) => this + oldOrigin - newOrigin;
 }
--- src/Granite.Engine/Primitives/Rect.cs
+++ src/Granite.Engine/Primitives/Rect.cs
@@ -1,7 +1,103 @@
 namespace Granite.Engine.Primitives;
 
-public readonly record struct Rect(Point Origin, Size Size)
+public readonly record struct Rect
 {
-    public Point P1 => Origin;
-    public Point P2 => new(Origin.X + Size.Width - 1, Origin.Y + Size.Height - 1);
+    public Point P1 { get; }
+    public Point P2 { get; }
+
+    public Rect(Point p1, Point p2)
+    {
+        P1 = p1;
+        P2 = p2;
+    }
+
+    public Rect(Point origin, Size size)
+    {
+        P1 = origin;
+        P2 = new Point(
+            origin.X + size.Width - 1,
+            origin.Y + size.Height - 1);
+    }
+
+    public bool Contains(Point point)
+    {
+        return point.X >= P1.X &&
+               point.X <= P2.X &&
+               point.Y >= P1.Y &&
+               point.Y <= P2.Y;
+    }
+
+    public bool Intersects(Rect other)
+    {
+        return P1.X <= other.P2.X &&
+               P2.X >= other.P1.X &&
+               P1.Y <= other.P2.Y &&
+               P2.Y >= other.P1.Y;
+    }
+
+    public bool TryGetIntersection(Rect other, out Rect intersection)
+    {
+        Point p1 = new(
+            Math.Max(P1.X, other.P1.X),
+            Math.Max(P1.Y, other.P1.Y));
+
+        Point p2 = new(
+            Math.Min(P2.X, other.P2.X),
+            Math.Min(P2.Y, other.P2.Y));
+
+        if (p1.X > p2.X || p1.Y > p2.Y)
+        {
+            intersection = default;
+            return false;
+        }
+
+        intersection = new Rect(p1, p2);
+        return true;
+    }
+
+    public IEnumerable<Rect> Subtract(Rect other)
+    {
+        if (!TryGetIntersection(other, out var intersection))
+        {
+            yield return this;
+            yield break;
+        }
+
+        // Top part
+        if (P1.Y < intersection.P1.Y)
+        {
+            yield return new Rect(
+                new Point(P1.X, P1.Y),
+                new Point(P2.X, intersection.P1.Y - 1));
+        }
+
+        // Bottom part
+        if (intersection.P2.Y < P2.Y)
+        {
+            yield return new Rect(
+                new Point(P1.X, intersection.P2.Y + 1),
+                new Point(P2.X, P2.Y));
+        }
+
+        // Left part
+        if (P1.X < intersection.P1.X)
+        {
+            yield return new Rect(
+                new Point(P1.X, intersection.P1.Y),
+                new Point(intersection.P1.X - 1, intersection.P2.Y));
+        }
+
+        // Right part
+        if (intersection.P2.X < P2.X)
+        {
+            yield return new Rect(
+                new Point(intersection.P2.X + 1, intersection.P1.Y),
+                new Point(P2.X, intersection.P2.Y));
+        }
+    }
+
+    public Rect Translate(Point offset) => new(P1.Translate(offset), P2.Translate(offset));
+
+    public Rect ChangeOrigin(Point oldOrigin, Point newOrigin) =>
+        new(P1.Rebase(oldOrigin, newOrigin), P2.Rebase(oldOrigin, newOrigin));
 }
